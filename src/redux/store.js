f7e5cb47b6656eb65afdbbf01158915b30aaa028
Oksana Kyryliuk <coksanna@gmail.com>
2023-02-25 15:02:52 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,6 +5,14 @@ import rootReducer from './root-reducer';
 
 export const store = configureStore({
   reducer: rootReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['your/action/type'],
+        ignoredActionPaths: ['meta.arg', 'payload.timestamp'],
+        ignoredPaths: ['items.dates'],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
